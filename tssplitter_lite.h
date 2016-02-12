31253142f22e111ec4c293552591a06f2dcb7977
rndomhack <rndom.hack@gmail.com>
2016-02-12 14:35:00 +0900

    Retain si pid

--- tssplitter_lite.h
+++ tssplitter_lite.h
@@ -48,6 +48,7 @@ typedef struct _pmt_version {
  */
 typedef struct _splitter {
 	unsigned char	pids[MAX_PID];
+	unsigned char	retain_pids[MAX_PID];
 	unsigned char	pmt_pids[MAX_PID];
 	unsigned char*	pat;
 	char**			sid_list;
